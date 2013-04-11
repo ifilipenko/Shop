4e6f7c67c84b20f66a2c1b648cdc10dfc2ff640c
Filipenko Innokentiy <filipenko.in@gmail.com>
2013-04-11 16:13:18 +0600

    Create logoff logic

--- src/Shop/Areas/Security/Controllers/LoginController.cs
+++ src/Shop/Areas/Security/Controllers/LoginController.cs
@@ -1,12 +1,26 @@
 ﻿using System.Web.Mvc;
+using System.Web.Security;
+using Shop.Areas.Security.Models;
 
 namespace Shop.Areas.Security.Controllers
 {
     public class LoginController : Controller
     {
-        public ActionResult LogOn()
+        public ActionResult CompactLogin()
         {
+            return View("CompactLogin");
+        }
+
+        public ActionResult LogOn(LogOnModel model)
+        {
+            FormsAuthentication.SetAuthCookie(model.Username, false);
             return RedirectToAction("Index", "Home", new {area = ""});
+        }
+
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home", new { area = "" });
         }
     }
 }
---
+++ src/Shop/Areas/Security/Models/LogOnModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Shop.Areas.Security.Models
+{
+    public class LogOnModel
+    {
+        public string Username { get; set; }
+    }
+}
