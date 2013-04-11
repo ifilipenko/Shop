c809a1f0743f72e54c67b493db1f67849dd055b2
Filipenko Innokentiy <filipenko.in@gmail.com>
2013-04-11 14:16:00 +0600

    Add twitter bootstrap and use _Layout via _ViewStart

---
+++ src/Shop/App_Start/BundleConfig.cs
@@ -0,0 +1,39 @@
+﻿using System.Web.Optimization;
+
+namespace Shop
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            LibsBundles(bundles);
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/bootstrap-responsive.css"));
+
+            bundles.Add(new StyleBundle("~/Content/Admin").Include(
+                "~/Content/Admin.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/tags-css").Include(
+                "~/Content/tags.css"));
+            bundles.Add(new StyleBundle("~/Content/tabs-css").Include(
+                "~/Content/tabs.css"));
+        }
+
+        private static void LibsBundles(BundleCollection bundles)
+        {
+            /*bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                "~/Scripts/modernizr-*"));*/
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-{version}.js"));
+
+            /*bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                "~/Scripts/jquery-ui-{version}.js"));*/
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/bootstrap.js"));
+        }
+    }
+}
