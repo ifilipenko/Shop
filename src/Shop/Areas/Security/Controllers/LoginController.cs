f9848c3778e1c8a8473c1c92854d356af6682560
Filipenko Innokentiy <filipenko.in@gmail.com>
2013-04-11 15:46:08 +0600

    Post username/password to server

---
+++ src/Shop/Areas/Security/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Shop.Areas.Security.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult LogOn()
+        {
+            return RedirectToAction("Index", "Home", new {area = ""});
+        }
+    }
+}
