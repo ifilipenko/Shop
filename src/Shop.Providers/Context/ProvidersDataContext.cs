2074f5c3a839d039a5755a51018fb781215703c7
Filipenko Innokentiy <filipenko.in@gmail.com>
2013-04-12 22:50:21 +0600

    Create shop.providers

---
+++ src/Shop.Providers/Context/ProvidersDataContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using Shop.Providers.Entities;
+
+namespace Shop.Providers.Context
+{
+    public class ProvidersDataContext : DbContext
+    {
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Conventions.Remove<System.Data.Entity.Infrastructure.IncludeMetadataConvention>();
+        //}
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+    }
+}
---
+++ src/Shop.Providers/Context/ProvidersDataContextInitializer.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using System.Web.Security;
+
+namespace Shop.Providers.Context
+{
+    public class ProvidersDataContextInitializer:DropCreateDatabaseAlways<ProvidersDataContext>
+    {
+        protected override void Seed(ProvidersDataContext context)
+        {
+            WebSecurity.Register("Demo", "123456", "demo@demo.com", true, "Demo", "Demo");
+            Roles.CreateRole("Admin");
+            Roles.AddUserToRole("Demo", "Admin");
+        }
+    }
+}
