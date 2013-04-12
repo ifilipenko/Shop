b034f98452492be6666d71c192fee898e404b87e
Filipenko Innokentiy <filipenko.in@gmail.com>
2013-04-12 12:04:44 +0600

    Add jquery validation and reference in Login view

--- src/Shop/App_Start/BundleConfig.cs
+++ src/Shop/App_Start/BundleConfig.cs
@@ -35,6 +35,10 @@ namespace Shop
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                 "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                "~/Scripts/jquery.unobtrusive*",
+                "~/Scripts/jquery.validate*"));
+
             /*bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                 "~/Scripts/jquery-ui-{version}.js"));*/
 
