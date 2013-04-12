1005f930d7c5c3b2095bd59208a14691fa991dde
Filipenko Innokentiy <filipenko.in@gmail.com>
2013-04-12 23:48:51 +0600

    Add new account registration

---
+++ src/Shop/Areas/Security/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Areas.Security.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Shop/Areas/Security/Models/RegisterModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Areas.Security.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтверждение пароля")]
+        public string ConfirmPassword { get; set; }
+    }
+}
