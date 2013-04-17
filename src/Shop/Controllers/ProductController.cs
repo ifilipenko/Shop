6f3df3ac1623fce6358801b51a6c1cbc9f25e2b2
Filipenko Innokentiy <filipenko.in@gmail.com>
2013-04-17 17:10:20 +0600

    Fake product controller create and edit

---
+++ src/Shop/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using System.Web.Mvc;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static int _lastProductId;
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View("CreateOrEdit", new EditProduct());
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            ViewBag.notice = TempData["notice"];
+            var model = new EditProduct
+                {
+                    Id = id,
+                    Name = "Product " + id,
+                    Category = "Category " + id,
+                    Description = "Description " + id,
+                    Vendor = "Vendor " + id
+                };
+            return View("CreateOrEdit", model);
+        }
+
+        [HttpPost]
+        public ActionResult Save(EditProduct model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("CreateOrEdit", model);
+            }
+
+            if (model.IsEditMode)
+            {
+                ViewBag.notice = "Товар успешно изменен";
+                return RedirectToAction("Edit", "Product", model.Id);
+            }
+
+            TempData["notice"] = "Товар успешно создан";
+            return RedirectToAction("Edit", "Product", ++_lastProductId);
+        }
+    }
+}
---
+++ src/Shop/Models/EditProduct.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace Shop.Models
+{
+    public class EditProduct
+    {
+        public bool IsEditMode
+        {
+            get { return Id == 0; }
+        }
+
+        public int Id { get; set; }
+
+        [DisplayName("Наименование")]
+        public string Name { get; set; }
+
+        [DisplayName("Категория")]
+        public string Category { get; set; }
+
+        [DisplayName("Производитель")]
+        public string Vendor { get; set; }
+
+        [DisplayName("Описание")]
+        public string Description { get; set; }
+    }
+}
