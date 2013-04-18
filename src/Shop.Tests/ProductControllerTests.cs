601dc092c68527b2927bfe3a71c393fabb02057c
Filipenko Innokentiy <filipenko.in@gmail.com>
2013-04-18 14:00:27 +0600

    Add tests for ProductController

---
+++ src/Shop.Tests/ProductControllerTests.cs
@@ -0,0 +1,73 @@
+﻿using System.Web.Mvc;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using Shop.Controllers;
+using Shop.Models;
+using Shop.Services.Domain;
+using Shop.Services.Domain.Commands;
+
+namespace Shop.Tests
+{
+    [TestFixture]
+    public class ProductControllerTests
+    {
+        private ProductController _productController;
+        private IProductService _productService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _productService = Substitute.For<IProductService>();
+            _productController = new ProductController(_productService);
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        public void Save_should_save_product(int id)
+        {
+            _productController.Save(new EditProduct {Id = id});
+
+            _productService.Received(1).SaveProduct(Arg.Is<ProductSaveCommand>(x => x.Id == id));
+        }
+
+        [TestCase(0, 1)]
+        [TestCase(1, 1)]
+        public void Save_should_redirect_to_edit_when_save_exists_product(int id, int returnedId)
+        {
+            _productService.SaveProduct(Arg.Any<ProductSaveCommand>()).Returns(returnedId);
+
+            var actionResult = _productController.Save(new EditProduct { Id = id });
+
+            actionResult.Should().BeOfType<RedirectToRouteResult>();
+            var routeResult = actionResult.As<RedirectToRouteResult>();
+            routeResult.RouteValues["id"].Should().Be(returnedId);
+            routeResult.RouteValues["action"].Should().Be("Edit");
+            routeResult.RouteValues["controller"].Should().Be("Product");
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        public void Save_should_redirect_to_view_when_model_is_invalid(int id)
+        {
+            _productController.ModelState.AddModelError("", "some error");
+            var model = new EditProduct {Id = id};
+
+            var actionResult = _productController.Save(model);
+
+            actionResult.Should().BeOfType<ViewResult>();
+            actionResult.As<ViewResult>().Model.Should().BeSameAs(model);
+            actionResult.As<ViewResult>().ViewName.Should().Be("CreateOrEdit");
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        public void Save_should_set_notice(int id)
+        {
+            _productController.Save(new EditProduct {Id = id});
+
+            _productController.TempData.Should().ContainKey("notice");
+            _productController.TempData["notice"].Should().NotBeNull();
+        }
+    }
+}
